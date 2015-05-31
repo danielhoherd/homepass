0a877c2405922904db552b3a997b68018e296a4b
Daniel Hoherd <daniel.hoherd@gmail.com>
2015-05-31 12:34:02 -0700

    Adding some files

--- RaspberryPi/setup.sh
+++ RaspberryPi/setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -x
-#
-# Set up spillpass pi functionality on base Raspian
+# HeadURL: https://github.com/danielhoherd/homepass/blob/master/RaspberryPi/setup.sh
+# Set up homepass functionality on base Raspian
+# So quick, so dirty. So dirty, in fact, that it doesn't actually work all the way.
 
-apt-get install -y git bridge-utils && \
-wget https://github.com/jenssegers/RTL8188-hostapd/archive/v2.0.tar.gz && \
-tar -zxvf v2.0.tar.gz && \
-cd RTL8188-hostapd-2.0/hostapd && \
-make && make install
+apt-get install -y git sqlite3 screen vim wget bridge-utils
 
 brctl addbr br0
 brctl addif br0 eth0
