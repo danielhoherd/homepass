be8b584517a016fdc9abf2a6706b58607607ad91
Daniel Hoherd <daniel.hoherd@gmail.com>
2015-05-31 21:32:36 -0700

    Adding -d, updating vars, adding warning

--- sanitize-csv.py
+++ sanitize-csv.py
@@ -4,17 +4,19 @@
 Tidy's up the csv syntax and de-dupes mac,ssid pairs.
 """
 
+print('This does not work yet.')
+
 import csv
 import sqlite3
 import argparse
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('-i', metavar='input filename', help="input csv file", default="maclist.csv")
-parser.add_argument('-o', metavar='output filename', help="output csv file", default="maclist-new.csv")
+parser.add_argument('-i', metavar='infile', help="input csv file", default="maclist.csv")
+parser.add_argument('-o', metavar='outfile', help="output csv file", default="maclist-new.csv")
+parser.add_argument('-d', metavar='database', help="temp database file", default=":memory:")
 args = parser.parse_args()
 
-# db = sqlite3.connect(':memory:')
-db = sqlite3.connect('foo.db')
+db = sqlite3.connect(args.d)
 
 with open('homepass.sql') as f:
     sql = f.read()
