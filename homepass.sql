9b0915ddd44ca6d112674a4c5852e14a286ae8e9
Daniel Hoherd <daniel.hoherd@gmail.com>
2015-05-31 19:52:20 -0700

    Adding in this safety until we have a better sanitizing method

--- homepass.sql
+++ homepass.sql
@@ -12,3 +12,4 @@ CREATE TABLE aps(
 -- The following will not import any fields for mac,ssid pairs that already exist in the db
 .mode csv
 .import maclist.csv aps
+delete from aps where MAC = 'MAC' ;
