034556b54d54bbf921f19d4ed64c389bb57d7496
Daniel Hoherd <daniel.hoherd@gmail.com>
2016-05-12 20:16:15 -0700

    Attempting a file resource

--- RaspberryPi/manifests/site.pp
+++ RaspberryPi/manifests/site.pp
@@ -4,3 +4,12 @@
 # systemctl enable homepass
 
 
+file { '/etc/network/interfaces':
+  ensure  => 'file',
+  group   => '0',
+  mode    => '644',
+  owner   => '0',
+  type    => 'file',
+  source  => 'puppet:///files/interfaces'
+}
+
