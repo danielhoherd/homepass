a3914d8752178aa5f9d1c682cc728860dbe6abe4
Daniel Hoherd <daniel.hoherd@gmail.com>
2016-05-17 19:54:06 -0700

    Fixing IF NOT EXISTS

--- homepass.sql
+++ homepass.sql
@@ -1,14 +1,14 @@
 -- https://github.com/danielhoherd/homepass/blob/master/homepass.sql
 
 PRAGMA foreign_keys=OFF;
-CREATE TABLE aps(
+CREATE TABLE IF NOT EXISTS aps(
     mac text NOT NULL,
     ssid text NOT NULL,
     lat text,
     lon text,
     last_used text,
     PRIMARY KEY (mac,  ssid)
-) IF NOT EXISTS ;
+) ;
 
 -- The following will not import any fields for mac,ssid pairs that already exist in the db
 .mode csv
